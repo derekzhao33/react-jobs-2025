0eb8f9ff1b6909659b0377df6c854c79c193983b
dz33 <derekzhao33@gmail.com>
2025-10-13 19:48:42 -0700

    Comple jobs page and add ability to add jobs

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,37 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-    ],
-  },
-  {
-    path: '/jobs',
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <JobsPage />,
-      }
-    ]
-  }
-]);
+import NotFoundPage from './pages/NotFoundPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+
+    return;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: '/jobs', element: <JobsPage /> },
+        { path: '/add-job', element: <AddJobPage addJobSubmit={addJob}/>},
+        { path: '/jobs/:id', element: <JobPage />, loader: jobLoader},
+        { path: '*', element: <NotFoundPage /> }
+      ]
+    }
+  ]);
+
   return <RouterProvider router={router} />;
 }
 
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const JobListing = ({job}) => {
   const [showFullDesc, setShowFullDesc] = useState(false);
 
   let description = job.description;
+  let descLength = description.length;
 
   if (!showFullDesc) {
-    description = description.substring(0, 90) + '...';
+    if (descLength > 90) {
+      description = description.substring(0, 90) + '...';
+    }
   } 
 
   return (
@@ -22,9 +26,13 @@ const JobListing = ({job}) => {
 
           <div className="mb-5">{description}</div>
 
-          <button onClick={() => setShowFullDesc((prevState) => (!prevState))} className="text-indigo-500 mb-5 hover:text-indigo-600">
-            { showFullDesc ? 'Less' : 'More' }
-          </button>
+          {descLength < 90 ? <></>
+            :
+            <button onClick={() => setShowFullDesc((prevState) => (!prevState))} className="text-indigo-500 mb-5 hover:text-indigo-600">
+              { showFullDesc ? 'Less' : 'More' }
+            </button>
+          }
+
 
           <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
@@ -35,12 +43,12 @@ const JobListing = ({job}) => {
               <FaMapMarker className='inline text-lg mb-1 mr-1'/>
               {job.location}
             </div>
-            <a
-              href={`/job/${job.id}`}
+            <Link
+              to={`/jobs/${job.id}`}
               className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
             >
             Read More
-            </a>
+            </Link>
           </div>
         </div>
       </div>
